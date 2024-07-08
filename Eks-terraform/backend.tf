298f41edfdf0ff6cd69e6ec0a8d8319713ce81e4
Rishabh bansal <166081637+Sirbansal@users.noreply.github.com>
2024-07-08 17:54:13 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sirbansal" # Replace with your actual S3 bucket name
+    bucket = "sirbansal1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
